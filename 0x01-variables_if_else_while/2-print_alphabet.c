223377fd7d6b5bdd317158de36a511d76e5230db
Nahashon <nahashon.ndirangu@outlook.com>
2022-09-15 05:36:14 -0700

    Variable if else while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Main entry point
+ *
+ * Return: Always Return 0
+ */
+
+int main(void)
+
+{
+	int n;
+		for (n = 97; n < 123; n++)
+		putchar(n);
+return  (0);
+}
