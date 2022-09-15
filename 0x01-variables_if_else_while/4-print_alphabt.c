6297e7f0210287567d4210bbe250581377bd4220
Nahashon <nahashon.ndirangu@outlook.com>
2022-09-15 06:12:50 -0700

    Variable if else while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Main entry point
+ *
+ * Return: Always Return 0
+ */
+
+int main(void)
+
+{
+	int n;
+
+	n = 97;
+	while (n < 123)
+		{if (n != 101 || n != 113)	
+			putchar(n);
+		n++; }
+putchar('\n');
+return  (0);
+}
+
