b82791aaa01ae1e43fe6f7f001696490f4ead55b
Nahashon <nahashon.ndirangu@outlook.com>
2022-09-15 07:55:42 -0700

    Functions-Nested Loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	int n;
+
+	for (n = 97; n < 123; n++)
+		putchar(n);
+	return (0);
+}
+
