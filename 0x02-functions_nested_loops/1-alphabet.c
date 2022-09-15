08e4f2222eba52039e818665a959692ebe8e3bb5
Nahashon <nahashon.ndirangu@outlook.com>
2022-09-15 15:19:47 -0700

    Functions Nested Loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - Prints lower case alphabets
  *
  * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
 {
-
 	int n;
 
 	for (n = 97; n < 123; n++)
 		_putchar(n);
-	_putchar('\n');	
+	_putchar('\n');
 }
