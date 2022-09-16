652391b906157a4a1a328b25c6899bf43d24ff25
Nahashon <nahashon.ndirangu@outlook.com>
2022-09-15 18:14:45 -0700

    Debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0)
 	{
-		if (month >= 2 && day >= 60)
+		if (month >= 2 && day >= 31)
 		{
 			day = convert_day(month,day);
 		}
@@ -23,13 +23,13 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month >= 2 && day >= 60)
+		if (month >= 2 && day >= 31)
 		{
 		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else
 		{
-		day = convert_day(mont, day);
+		day = convert_day(month, day);
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 365 - day);
 		}
