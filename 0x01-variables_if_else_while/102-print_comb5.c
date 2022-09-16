6313da893023699438259576b0d68f65015b0078
Nahashon <nahashon.ndirangu@outlook.com>
2022-09-16 02:39:58 -0700

    If else while

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c, d;
+
+	for (c = 0; c < 99; c++)
+	{
+		for (d = 0; d < 100; d++)
+		{
+			putchar(c / 10 + '0');
+			putchar(c % 10 + '0');
+			putchar(' ');
+			putchar(d / 10 + '0');
+			putchar(d % 10 + '0');
+			if (c == 98 && d == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
+
