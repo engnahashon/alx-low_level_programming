2217e5de73ef2f18c61941bd1d8078b8bdc7902a
Nahashon <nahashon.ndirangu@outlook.com>
2022-09-16 11:26:22 -0700

    functions nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,16 +6,16 @@
  */
 void times_table(void)
 {
-	int a, b;
+	int a, b, mult;
 
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 10; b++)
 		{
-			printf("%d", a * b);
-			if (b < 9)
-			printf(", ");
+			mult = a * b;
+			putchar(mult);
+			
 		}
-		printf("\n");
+		putchar('\n');
 	}
 }
