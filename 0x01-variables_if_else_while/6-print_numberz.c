5c4592a6b2c880154edbcd376f985ab010da76d6
Nahashon <nahashon.ndirangu@outlook.com>
2022-09-16 01:08:08 -0700

    If else while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 0; c < 10; c++)
+	putchar(c % 10 + '0');
+	putchar('\n');
+
+	return (0);
+}
+
