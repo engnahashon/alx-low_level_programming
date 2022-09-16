203c743132ea97a32d711b7c080b265c6900c78e
Nahashon <nahashon.ndirangu@outlook.com>
2022-09-16 01:20:53 -0700

    If else while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Main entry point
+ *
+ * Return: Always Return 0
+ */
+
+int main(void)
+
+{
+	int n;
+		for (n = 122; n < 96; n--)
+		putchar(n);
+putchar('\n');
+return  (0);
+}
+
