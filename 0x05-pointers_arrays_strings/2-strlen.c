dd6dbfe119c6f5a4625f8e3bb620d81460f553f6
Nahashon <nahashon.ndirangu@outlook.com>
2022-09-20 01:32:29 -0700

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to char
+ *
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+	char str[] = *s;
+	return (strlen(str));
+}
