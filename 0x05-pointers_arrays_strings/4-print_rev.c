817ba768006385635d4629cc88dc59dad4f62cb9
Nahashon <nahashon.ndirangu@outlook.com>
2022-09-20 03:08:20 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,11 @@
  */
 void print_rev(char *s)
 {
-	int len, n;
+	int len = 0, n;
 	
 	while (*s)
 	  len++;
-      	for (n = len; n >= 0; n--)
+      	for (n = len - 1; n >= 0; n--)
 		_putchar(*s[n]);
 	_putchar('\n');
 }
