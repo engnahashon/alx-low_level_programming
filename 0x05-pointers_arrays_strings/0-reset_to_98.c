ad67eebb56db9035955cad09502a8493a54279c0
Nahashon <nahashon.ndirangu@outlook.com>
2022-09-20 01:01:20 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to_98 - takes a pointer to an int as parameter
  * updates the value it points to to 98
- * @*n: pointer to an interger
+ * @n: pointer to an interger
  *
  * Return: value of integer
  */
