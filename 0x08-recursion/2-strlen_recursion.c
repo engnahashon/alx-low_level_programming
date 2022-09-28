9124cd1dd4217759315b47322f1523be2193e141
Nahashon <nahashon.ndirangu@outlook.com>
2022-09-28 07:47:54 -0700

    Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * _strlen_recursion - returns length of string
+  *  @s: string
+  * 
+  * Return: length of string
+  */
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	if (*s != '\0')
+	{
+		len = 1 + _strlen_recursion(s + 1);
+	}
+	return (len);
+}
+
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int _pow_recursion(int x, int y);
+int factorial(int n);
+int _strlen_recursion(char *s);
 void _print_rev_recursion(char *s);
 void _putchar(char c);
 void _puts_recursion(char *s);
