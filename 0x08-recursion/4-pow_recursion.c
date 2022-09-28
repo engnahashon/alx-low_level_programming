d86f2b8d2c66e4aaadc9d302cc58e9d47bab0ef6
Nahashon <nahashon.ndirangu@outlook.com>
2022-09-28 13:48:57 -0700

    Recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,14 +10,11 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	else if (y == 0)
+	if (y == 0)
 		return (1);
+	if (y == 1)
+		return (x * 1);
 	else
-	{
-		if (y == 1)
-			return (x * 1);
-		else
-			return (x * _pow_recursion(x, y - 1));
+		return (x * _pow_recursion(x, y - 1));
 
-	}
 }
