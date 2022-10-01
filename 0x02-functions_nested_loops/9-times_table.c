d9b1f96363a64e140bb3859993a6d868bbb43438
Nahashon <nahashon.ndirangu@outlook.com>
2022-10-01 01:24:28 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,22 +10,23 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		_putchar('0');
 		for (b = 0; b <= 9; b++)
 		{
-			_putchar(',');
-			_putchar(' ');
 
 			mult = a * b;
 			if (mult < 10)
 			{
-				_putchar(' ');
 				_putchar((mult % 10) + '0');
 			}
 			else
 			{
 				_putchar((mult / 10) + '0');
 				_putchar((mult % 10) + '0');
+			}
+			if(b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 /**
  * main - check the code for Holberton School students.
  *
@@ -7,14 +7,13 @@ include "main.h"
 int main(void)
 
 {
-	void times_table(void);
-	void jack_bauer(void);
-	int print_last_digit(int);
-	int _abs(int);
-	int print_sign(int n);
-	int _isalpha(int c);
-	int _islower(int c);
-	void print_alphabet(void);
-	void print_alphabet_x10(void);
+	int  n = 1, i;
+	
+	for (i = 0; i < 15; i++)
+	{
+	_putchar(n + '1');
+	_putchar('\n');
+	n = n  + 1;
+	}
 	return (0);
 }
