45ddb2eee3303d8932cddd04117957d3ad5c132b
Nahashon <nahashon.ndirangu@outlook.com>
2022-10-01 04:28:27 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,15 @@
 #include "main.h"
 /**
+  * main - main entry
+  *
+  * Return: zero
+  */
+int main(void)
+{
+	multiples();
+	return(0);
+}
+/**
   * multiples -  computes the sum of all the multiples of 3 or 5 below 1024
   */
 void multiples(void)
