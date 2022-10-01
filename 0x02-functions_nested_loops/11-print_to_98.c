ef9a4e754673f2b9eaaf0157a27e22db082692c3
Nahashon <nahashon.ndirangu@outlook.com>
2022-10-01 02:31:42 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,10 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i == 98)
+				continue;
+			printf(", ");
 		}
 		printf("\n");
 	}
@@ -24,6 +27,9 @@ void print_to_98(int n)
 		for (i = n; i >= 98; i--)
 		{
 			printf("%d, ", i);
+			if (i == 98)
+				continue;
+			printf(", ");
 		}
 		printf("\n");
 	}
