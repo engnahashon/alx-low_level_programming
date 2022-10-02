77d17dcccdcd2853d32c2a9d7776508898213004
Nahashon <nahashon.ndirangu@outlook.com>
2022-10-02 08:42:58 -0700

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -22,7 +22,9 @@ void fizzbuzz(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if (n % 3 == 0)
+		if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz ");
+		else if (n % 3 == 0)
 			printf("Fizz ");
 		else if (n % 5 == 0)
 		{
@@ -31,8 +33,6 @@ void fizzbuzz(void)
 			else
 				printf("Buzz ");
 		}
-		else if (n % 3 == 0 && n % 5 == 0)
-			printf("FizzBuzz ");
 		else
 			printf("%d ", n);
 	}
