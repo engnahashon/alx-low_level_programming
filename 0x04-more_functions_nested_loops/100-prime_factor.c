04d761268fd1ec8b69dffe8098d73011b12cf72f
Nahashon <nahashon.ndirangu@outlook.com>
2022-10-02 10:49:14 -0700

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -19,21 +19,7 @@ void prime_num(void)
 {
 	long int num = 612852475143;
 	long int i;
-
-/*
-	while (num != 1)
-	{
-		if (num % i == 0)
-		{
-			num = num / i;
-			if (i > factor)
-				factor = i;
-
-		}
-		else
-			factor++;
-	}
-*/
+	
 	for (i = 3; i < 782849; i = i + 2)
 	{
 		while ((num % i == 0) && (num != i))
