c53f38ebf67635cf2444b07bd91093850aaaaf14
Nahashon <nahashon.ndirangu@outlook.com>
2022-10-04 02:37:23 -0700

    Memory allocation and free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
-  * creates an array of chars, and initializes it with a specific char.
+  * create_array - creates an array of chars
+  * and initializes it with a specific char.
   * @size: size of array
   * @c: Charater to initiliaze
   *
@@ -10,7 +11,8 @@ char *create_array(unsigned int size, char c)
 {
 	char *str;
 	unsigned int i;
-
+	if (size == 0)
+		return (NULL);
 	str = malloc(sizeof(str) * size);
 	if (str == NULL)
 		return (NULL);
