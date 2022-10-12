9b2206772653cfe1ab39c6cfd6954195441bf9da
engnahashon <nahashon.ndirangu@outlook.com>
2022-10-12 09:20:41 +0000

    Static Libraries

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - multiplies two numbers
+  * @argc: arg count
+  * @argv: arg vector
+  *
+  * Return: zero on success, 1 on error
+  */
+int main(int argc, char *argv[])
+{
+	int mult;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		mult = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mult);
+	}
+	return (0);
+}
