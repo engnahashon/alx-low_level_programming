0e84b632372ea74d82d463fe48d21f1852480b7e
engnahashon <nahashon.ndirangu@outlook.com>
2022-10-15 09:27:32 +0000

    Variadic Functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,19 +1,20 @@
 #include "variadic_functions.h"
 /**
-  * sum_thme_allc- sum of allcits paraameters
-  * @n: numm of oara \
+  * sum_them_all - sum of all its parameters
+  * @n: num of arg
+  *
+  * Return: Sum of arg
   */
 int sum_them_all(const unsigned int n, ...)
-{	int sum = 0;
+{
+	int sum = 0;
 	unsigned int i;
 	va_list valist;
 
 	va_start(valist, n);
-	
-	for (i = 0; i < n; i++)
 
+	for (i = 0; i < n; i++)
 		sum += va_arg(valist, int);
 	va_end(valist);
-
 	return (sum);
 }
