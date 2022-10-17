5f8d1553e53f5565120484e0fedb90984647ebdf
engnahashon <nahashon.ndirangu@outlook.com>
2022-10-17 16:17:12 +0000

    Function Pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+/**
+  * main - Performs simples operations
+  * @argc: arg count
+  * @argv: arg vector
+  *
+  * Return: zero on success.
+  */
+int main(int argc, char *argv)
+{
+	int a, b;
+	int (*p)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	p = get_op_func(argv[2]);
+	if (p == NULL)
+	{
+		printf("Error\n");
+		return(99);
+	}
+	printf("%d\n", (*p)(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,82 @@
+#include "3-calc.h"
+/**
+  * op_add - sum of a and b
+  * @a: a
+  * @b: b
+  *
+  * Return: sum
+  */
+int op_add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
+/**
+  * op_sub - difference of a and b
+  * @a: a
+  * @b: b
+  *
+  * Return: sub
+  */
+int op_sub(int a, int b)
+{
+	int sub;
+
+	sub = a + b;
+	return (sub);
+}
+/**
+  * op_mul - mul of a and b
+  * @a: a
+  * @b: b
+  *
+  * Return: mul
+  */
+int op_mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
+/**
+  * op_div - div of a by b
+  * @a: a
+  * @b: b
+  *
+  * Return: div
+  */
+int op_div(int a, int b)
+{
+	int div;
+
+	if (b == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+	div = a / b;
+	return (div);
+}
+/**
+  * op_mod - mod of a and b
+  * @a: a
+  * @b: b
+  *
+  * Return: mod
+  */
+int op_mod(int a, int b)
+{
+	int mod;
+
+	if (b == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	mod = a + b;
+	return (mod);
+}
