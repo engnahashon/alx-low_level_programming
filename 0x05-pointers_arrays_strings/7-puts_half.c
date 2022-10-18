9768426cd595433e87159afaa8d1e2e6d9944708
engnahashon <nahashon.ndirangu@outlook.com>
2022-10-18 08:55:06 +0000

    Pointers, Arrays & Strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,23 @@
 #include "main.h"
 /**
+  * get_str_len - Gets string length
+  * @str: string
+  *
+  * Return: int str len
+  */
+int get_str_len (char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
+
+/**
   * puts_half - prints half of a string
   * @str: string to output
   */
