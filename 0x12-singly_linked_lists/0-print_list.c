a3b1608b6d02ea05e4d9abf3b75a04ee4b440a0c
engnahashon <nahashon.ndirangu@outlook.com>
2022-10-25 05:20:24 +0000

    Singly Linked Lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,17 +11,12 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (h == NULL)
-		{
-			printf("[0] (nil)");
-			return (0);
-		}
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-			i++;
-		}
+		h = h->next;
+		i++;
 	}
 	return (i);
 }
