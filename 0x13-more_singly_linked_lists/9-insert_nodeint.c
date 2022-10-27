a7833c2ae46190bbfa8c7b85dc51cfd0aacbd338
engnahashon <nahashon.ndirangu@outlook.com>
2022-10-27 22:33:51 +0000

    More Singly Linked Lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,12 +13,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new;
 
 	temp = *head;
+	if (temp == NULL)
+		return (NULL);
 	while (i < idx)
 	{
 		temp = temp->next;
 		i++;
 	}
 	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 	new->n = n;
 	new->next = temp->next;
 	temp->next = new;
