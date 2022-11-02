f60e396b396cbf1457db4a5cfb1aae743a257ffd
engnahashon <nahashon.ndirangu@outlook.com>
2022-11-02 12:26:47 +0000

    File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -34,7 +34,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	op = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+	op = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (op == -1)
 	{
 		close(op);
