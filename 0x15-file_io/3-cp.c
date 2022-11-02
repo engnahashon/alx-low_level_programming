7b1f5ed8ec0f796140aa0766d02107c66eee14dd
engnahashon <nahashon.ndirangu@outlook.com>
2022-11-02 21:24:37 +0000

    File I/O

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,63 @@
+#include "main.h"
+/**
+  * main - copies the content of a file to another file
+  * @argc: argument count
+  * @argv: argument variable
+  *
+  * Return: 0 on success
+  */
+int main(int argc, char *argv[])
+{
+	int op, rd, wr, cl;
+	char *buf;
+	int count = 1024;
+
+	buf = malloc(count);
+
+	if (argc != 3)
+	{
+		printf("Usage: cp file_from file_to\n");
+		free(buf);
+		exit(97);
+	}
+
+	op = open(argv[1], O_RDONLY);
+	if (op == -1)
+	{
+		printf("Error: Can't read from file %s\n", argv[1]);
+		free(buf);
+		exit(99);
+	}
+	rd = read(op, buf, count);
+	if (rd == -1)
+	{
+		printf("Error: Can't read from file %s\n", argv[1]);
+		free(buf);
+		exit(99);
+	}
+	cl = close(op);
+	if (cl == -1)
+	{
+		printf("Error: Can't close %d\n", op);
+		free(buf);
+		exit(100);
+	}
+	op = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	wr = write(op, buf, count);
+	if (wr == -1)
+	{
+		printf("Error: Can't write to file %s\n", argv[2]);
+		free(buf);
+		exit(99);
+	}
+	free(buf);
+	cl = close(op);
+	if (cl == -1)
+	{
+		printf("Error: Can't close %d\n", op);
+		free(buf);
+		exit(100);
+	}
+	
+	return (0);
+}
