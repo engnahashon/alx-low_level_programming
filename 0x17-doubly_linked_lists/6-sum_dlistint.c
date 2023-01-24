e8e841ae0a80f9c8bf1d6b878443672990a05022
engnahashon <nahashon.ndirangu@outlook.com>
2023-01-24 11:07:24 +0000

    C-Doubly Linked Structures

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+  * sum_dlistint - returns sum of all the data n of a dlistint
+  * @head: Head of the d list
+  *
+  * Return: sum, 0 if empty
+  */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	else
+	{
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+	}
+}
