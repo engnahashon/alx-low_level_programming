db9e15f6bcbfb53e1d83630d6f399895685879eb
engnahashon <nahashon.ndirangu@outlook.com>
2023-01-24 07:50:13 +0000

    C-Doubly Linked Structures

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,17 +1,16 @@
 #include "lists.h"
 /**
- * print_dlistint - prints all the elements of a dlistint_t list
+ * dlistint_len - Returns elements of a dlistint_t list
  * @h: head of the doubly linked list
  *
- * Return: the number of nodes
+ * Return: the number of elements
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
     size_t count = 0;
 
     while (h)
     {
-        printf("%d\n", h->n);
         h = h->next;
         count++;
     }
