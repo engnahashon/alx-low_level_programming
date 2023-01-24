df67988b7077380dc03f76ce9f02af9ad7083cf4
engnahashon <nahashon.ndirangu@outlook.com>
2023-01-24 09:40:05 +0000

    C-Doubly Linked Structures

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+  * free_dlistint - free dlistint
+  * *head: Head of doubly linked lined
+  */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+
+	while (current != NULL)
+	{
+		free(current);
+		current = current->next;
+	}
+}
