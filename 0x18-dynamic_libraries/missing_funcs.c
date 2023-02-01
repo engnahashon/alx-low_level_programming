fcee9f6811dd71dd34ee75051a96ae112973adef
engnahashon <nahashon.ndirangu@outlook.com>
2023-02-01 11:01:15 +0000

    Dynamic Libraries

---
+++ 0x18-dynamic_libraries/missing_funcs.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+int _atoi(char *s)
+{
+    return 0;
+}
+
+char *_strcat(char *dest, char *src)
+{
+    return dest;
+}
+
+char *_strncat(char *dest, char *src, int n)
+{
+    return dest;
+}
+
+char *_strncpy(char *dest, char *src, int n)
+{
+    return dest;
+}
+
+int _strcmp(char *s1, char *s2)
+{
+    return 0;
+}
+
+char *_strpbrk(char *s, char *accept)
+{
+    return s;
+}
+
+char *_strstr(char *haystack, char *needle)
+{
+    return haystack;
+}
