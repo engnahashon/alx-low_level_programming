d36e55bc15cceaed40f97e8ce7ebb218e08ab615
engnahashon <nahashon.ndirangu@outlook.com>
2023-02-01 10:52:13 +0000

    Dynamic Libraries

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    printf("%ld\n", strlen("My Dyn Lib"));
+    return (EXIT_SUCCESS);
+}
