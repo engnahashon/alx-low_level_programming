ab4000cfb193e051c4b5a285f803cdd64ce19558
engnahashon <nahashon.ndirangu@outlook.com>
2023-02-24 12:37:45 +0000

    island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    """ Returns the perimeter of an island"""
+
+    if not isinstance(grid, list):
+        return 0
+    p = 0
+
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                p += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    p -= 1
+                if col > 0 and grid[row][col - 1] == 1:
+                    p -= 1
+                if col < (len(grid[row]) - 1) and grid[row][col + 1] == 1:
+                    p -= 1
+                if row < (len(grid) - 1) and grid[row + 1][col] == 1:
+                    p -= 1
+    return p
