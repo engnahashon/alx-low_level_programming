c3cb05c8b56a2e1f7c9e68cb9d2eb7e6a9536833
engnahashon <nahashon.ndirangu@outlook.com>
2023-04-13 21:51:25 +0000

    binary

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,17 +11,23 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t idx;
+	size_t idx = 0;
+	size_t L = 0;
+	size_t R = size - 1;
 
 	if (!array)
 		return (-1);
-
-	L = 0;
-	R = size - 1;
-
+	
 	while (L <= R)
 	{
 		int m = floor((L+R)/2);
+		if (array[m] < value)
+			L = m + 1;
+		else if (array[m] > value)
+			R = m - 1;
+		else
+			return (idx);
 	}
+	return (-1);
 
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,7 +2,7 @@
 #define _SEARCH_ALGOS_H_
 
 #include <stdio.h>
-
+#include <math.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
