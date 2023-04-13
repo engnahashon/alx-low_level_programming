2c733f9c0baa0d56e657e51e0fdec23e5b3557b3
engnahashon <nahashon.ndirangu@outlook.com>
2023-04-13 21:08:07 +0000

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,7 +16,7 @@ int linear_search(int *array, size_t size, int value)
 
 	while (idx < size)
 	{
-		printf("Value checked array[%lu] = %d\n", idx, array[idx]);
+		printf("Value checked array[%lu] = [%d]\n", idx, array[idx]);
 		if (array[idx] != value)
 		{
 			idx++;
